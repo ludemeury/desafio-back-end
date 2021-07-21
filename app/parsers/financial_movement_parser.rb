e342e8ce6f4eee3948fd4d75164d14e9c5399e1f
Ludemeury Fernandes de Sá <ludemeury@gmail.com>
2021-07-21 01:14:42 -0300

    feat: add financial movement parser

---
+++ app/parsers/financial_movement_parser.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class FinancialMovementParser
+  def self.parse(document_lines)
+    Rails.logger.info("#{Time.now.strftime('%F %T')} -  #{self}::#{__method__}")
+    return {} unless document_lines.is_a? Array
+
+    Rails.logger.info("#{Time.now.strftime('%F %T')} -  #{self}::#{__method__} - #{document_lines.length}")
+
+    document_lines.collect do |line|
+      {
+        kind: line[0],
+        date: line[1..8],
+        value: line[9..18],
+        document: line[19..29],
+        card: line[30..41],
+        hour: line[42..47],
+        shop_owner: line[48..61].to_s.strip,
+        shop_name: line[62..79].to_s.strip
+      }
+    end
+  end
+end
