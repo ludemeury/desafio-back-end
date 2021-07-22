520e910ed88ceea3fe9ff485e6202ac660487c65
Ludemeury Fernandes de Sá <ludemeury@gmail.com>
2021-07-22 00:18:16 -0300

    test: owner model validations

--- spec/models/owner_spec.rb
+++ spec/models/owner_spec.rb
@@ -1,5 +1,20 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Owner, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Validations' do
+    it 'is not valid without a document' do
+      expect(Owner.create(name: 'lud')).to_not be_valid
+    end
+
+    it 'is not valid without an name' do
+      expect(Owner.create(document: '123')).to_not be_valid
+    end
+
+    it 'is not valid with repeated document' do
+      Owner.create(name: 'joao pereira', document: '12345')
+      expect(Owner.create(name: 'joao pereira', document: '12345')).to_not be_valid
+    end
+  end
 end
